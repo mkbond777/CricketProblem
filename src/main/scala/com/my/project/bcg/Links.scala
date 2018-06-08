d5c5c461158864dae1d25231ab251e721c0c1b0e
manish <m.kumar@in.imshealth.com>
2018-06-08 21:35:44 +0530

    Some useful links

---
+++ src/main/scala/com/my/project/bcg/Links.scala
@@ -0,0 +1,23 @@
+package com.my.project.bcg
+
+/**
+  * Created by DT2(M.Kumar) on 6/8/2018.
+  */
+class Links {
+
+  // Change Column Type
+  // https://stackoverflow.com/questions/29383107/how-to-change-column-types-in-spark-sqls-dataframe
+
+  // Scala Regex
+  // https://www.tutorialspoint.com/scala/scala_regular_expressions.htm
+
+  // Extract Year from Date Column
+  // https://forums.databricks.com/questions/9790/how-to-extract-year-and-week-number-from-a-columns.html
+
+  // Select max of each Group
+  // https://stackoverflow.com/questions/33878370/how-to-select-the-first-row-of-each-group/33878701#33878701
+
+  // Fold, Reduce and Scan
+  // https://stackoverflow.com/questions/17408880/reduce-fold-or-scan-left-right
+
+}
